b925d8341fe1a5f4cc27018efced70133ffea547
Tushar Thapa <tusharthapa790@gmail.com>
2026-04-17 21:17:26 +0545

    tushar commited for getting problem

--- src/main/java/com/artmarketplace/utilities/DBConnection.java
+++ src/main/java/com/artmarketplace/utilities/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/art_marketplace_db";
+    private static final String URL = "jdbc:mysql://localhost:3306/art_marketplace";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
