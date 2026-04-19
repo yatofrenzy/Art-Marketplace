c5aa1b31e128891583e7144e7cd6077417331bde
Tushar Thapa <tusharthapa790@gmail.com>
2026-04-19 23:01:52 +0545

    changed path of login.jsp to login for proper path properly

--- src/main/java/com/artmarketplace/controller/servlets/RegisterServlet.java
+++ src/main/java/com/artmarketplace/controller/servlets/RegisterServlet.java
@@ -68,7 +68,7 @@ public class RegisterServlet extends HttpServlet {
         boolean result = dao.registerUser(user);
 
         if (result) {
-            response.sendRedirect(request.getContextPath() + "/pages/common/login");
+            response.sendRedirect(request.getContextPath() + "/login");
         } else {
             request.setAttribute("error", "Registration failed");
             request.getRequestDispatcher("/pages/common/register.jsp").forward(request, response);
