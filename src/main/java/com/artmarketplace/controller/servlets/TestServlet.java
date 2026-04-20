9fe054035194843e262238c5e4f8e1780c9e4f95
nabrajmahatara <nabrajmahatara0@gmail.com>
2026-04-20 13:00:57 +0545

    Solved servlet

---
+++ src/main/java/com/artmarketplace/controller/servlets/TestServlet.java
@@ -0,0 +1,22 @@
+package com.artmarketplace.controller.servlets;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.getWriter().println("Test servlet working");
+    }
+}
