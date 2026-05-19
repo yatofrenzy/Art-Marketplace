7363725bd64e9608a3dcb041cfc826fc44029c1f
nabrajmahatara <nabrajmahatara0@gmail.com>
2026-05-19 22:34:26 +0545

    changed id pw of users

--- sql/seed.sql
+++ sql/seed.sql
@@ -1,5 +1,4 @@
 -- Art Marketplace Seed Data
--- Run this file after art_marketplace.sql
 
 USE art_marketplace;
 
@@ -21,11 +20,15 @@ ALTER TABLE users AUTO_INCREMENT = 1;
 
 SET FOREIGN_KEY_CHECKS = 1;
 
+-- Admin password = 123456789
+-- Customer Password = 123456789
 INSERT INTO users (user_id, name, email, password, role, contact_number, profile_image, account_status, phone) VALUES
-(1, 'Admin', 'admin@gmail.com', '123', 'admin', NULL, NULL, 'Approved', '9800000000'),
-(2, 'Customer', 'customer@gmail.com', '123', 'customer', '9819821233', NULL, 'Approved', '9819821233'),
-(3, 'Customer One', 'customer1@gmail.com', '123', 'customer', NULL, NULL, 'Approved', '9812340983'),
-(4, 'Customer Two', 'customer2@gmail.com', '123', 'customer', NULL, NULL, 'Pending', '9812340912');
+(1, 'Admin', 'admin@gmail.com', '123', '$2a$10$6cFa/jsQoPgXYs5kcSaQAuS3GMe2QEWDHUmqzW0Ib7nKTtDqwwk9C', NULL, NULL, 'Approved', '9800000000'),
+(2, 'Customer', 'customer@gmail.com', '$2a$10$k0clZAroNJK4kD4LC97Szear2qqmsBFv8ZQbx38dbsuZO/9lI3vm.', 'customer', '9819821233', NULL, 'Approved', '9819821233'),
+(3, 'Asmi', 'asmi@gmail.com', '$2a$10$M42roEiNaWR8TCfFN73kme/3u4g.75A1gDZQ1ScmzS4SofEoCKVKa', 'customer', NULL, NULL, 'Approved', '9812340983'),
+(4, 'Nabraj', 'nabraj@gmail.com', '$2a$10$gGZX7Z/X6Cim4.z/09VdneuRvL5Ya7Yvc.ofunygfeTrc.8R.Qreu', 'customer', NULL, NULL, 'Pending', '9812340912');
+(4, 'Samrat', 'samrat@gmail.com', '$2a$10$gGZX7Z/X6Cim4.z/09VdneuRvL5Ya7Yvc.ofunygfeTrc.8R.Qreu', 'customer', NULL, NULL, 'Pending', '9812340912');
+(4, 'Sudeshna', 'sudeshna@gmail.com', '$2a$10$NQlPuARk98ZQ/ba1R.lP3.P6Xcu5Tk4RGGIzbcDf9Dqoo9TJRbChG', 'customer', NULL, NULL, 'Pending', '9812340912');
 
 INSERT INTO categories (category_id, category_name) VALUES
 (1, 'Painting'),
